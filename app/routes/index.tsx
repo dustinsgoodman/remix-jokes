a13eda197c40cdb3a27990f6d362b84d7104249c
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-01 19:29:03 -0600

    apply stylesheet to root route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,10 @@
+import type { LinksFunction } from 'remix';
+import stylesUrl from '~/styles/index.css';
+
+export const links: LinksFunction = () => ([
+  { rel: 'stylesheet', href: stylesUrl },
+])
+
 export default function IndexRoute() {
   return <div>Hello Index Route</div>;
 }
