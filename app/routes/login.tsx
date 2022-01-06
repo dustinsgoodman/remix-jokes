0690f0943baa196db5ef25a95bdfcfd96404c6bd
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-05 20:39:15 -0600

    add login validation and scaffolded action handler

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,18 +1,108 @@
-import type { LinksFunction } from "remix";
-import { Link, useSearchParams } from "remix";
+import { ActionFunction, LinksFunction } from "remix";
+import { json, Link, redirect, useActionData, useSearchParams } from "remix";
+import { db } from "~/utils/db.server";
 import stylesUrl from "~/styles/login.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    username: string | undefined;
+    password: string | undefined;
+  };
+  fields?: {
+    loginType: string;
+    username: string;
+    password: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const loginType = form.get("loginType");
+  const username = form.get("username");
+  const password = form.get("password");
+  const redirectTo = form.get("redirectTo") || "/jokes";
+
+  if (
+    typeof loginType !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return badRequest({
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fields = { loginType, username, password };
+  const fieldErrors = {
+    username: username.length < 3 ? "Usernames must be at least 3 characters long" : undefined,
+    password: password.length < 6 ? "Passwords must be at least 6 characters long" : undefined,
+  };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+    });
+  }
+
+  switch (loginType) {
+    case "login": {
+      // login to get the user
+      // if there's no user, return the fields and a formError
+      // if there is a user, create their session and redirect to /jokes
+      return badRequest({
+        fields,
+        formError: "Not implemented"
+      });
+    }
+    case "register": {
+      const userExists = await db.user.findFirst({
+        where: { username }
+      });
+      if (userExists) {
+        return badRequest({
+          fields,
+          formError: `User with username ${username} already exists`
+        });
+      }
+      // create the user
+      // create their session and redirect to /jokes
+      return badRequest({
+        fields,
+        formError: "Not implemented"
+      });
+    }
+    default: {
+      return badRequest({
+        fields,
+        formError: `Login type invalid`
+      });
+    }
+  }
+};
+
 export default function Login() {
+  const actionData = useActionData<ActionData>();
   const [searchParams] = useSearchParams();
   return (
     <div className="container">
       <div className="content" data-light="">
         <h1>Login</h1>
-        <form method="post">
+        <form
+          method="post"
+          aria-describedby={
+            actionData?.formError
+              ? "form-error-message"
+              : undefined
+          }
+        >
           <input
             type="hidden"
             name="redirectTo"
@@ -29,7 +119,10 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="login"
-                defaultChecked
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData?.fields?.loginType === "login"
+                }
               />
               &nbsp;
               Login
@@ -39,6 +132,9 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="register"
+                defaultChecked={
+                  actionData?.fields?.loginType === "register"
+                }
               />
               &nbsp;
               Register
@@ -50,7 +146,25 @@ export default function Login() {
               type="text"
               id="username-input"
               name="username"
+              defaultValue={actionData?.fields?.username}
+              aria-invalid={Boolean(
+                actionData?.fieldErrors?.username
+              )}
+              aria-describedby={
+                actionData?.fieldErrors?.username
+                  ? "username-error"
+                  : undefined
+              }
             />
+            {actionData?.fieldErrors?.username ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="username-error"
+              >
+                {actionData?.fieldErrors.username}
+              </p>
+            ) : null}
           </div>
           <div>
             <label htmlFor="password-input">Password</label>
@@ -58,7 +172,37 @@ export default function Login() {
               id="password-input"
               name="password"
               type="password"
+              defaultValue={actionData?.fields?.password}
+              aria-invalid={
+                Boolean(
+                  actionData?.fieldErrors?.password
+                ) || undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.password
+                  ? "password-error"
+                  : undefined
+              }
             />
+            {actionData?.fieldErrors?.password ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="password-error"
+              >
+                {actionData?.fieldErrors.password}
+              </p>
+            ) : null}
+          </div>
+          <div id="form-error-message">
+            {actionData?.formError ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+              >
+                {actionData?.formError}
+              </p>
+            ) : null}
           </div>
           <button type="submit" className="button">
             Submit
