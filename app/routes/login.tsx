1b9b13fcb1011f5dc1b9d394d4604e1b57af3605
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-05 20:56:47 -0600

    update login to use session helper

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,8 @@
 import { ActionFunction, LinksFunction } from "remix";
-import { json, Link, redirect, useActionData, useSearchParams } from "remix";
+import { json, Link, useActionData, useSearchParams } from "remix";
 import { db } from "~/utils/db.server";
 import stylesUrl from "~/styles/login.css";
+import { login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -54,8 +55,14 @@ export const action: ActionFunction = async ({ request }) => {
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
+      const user = await login({ username, password });
+      if (!user) {
+        return badRequest({
+          formError: "Invalid username or password",
+          fields,
+        });
+      }
+
       // if there is a user, create their session and redirect to /jokes
       return badRequest({
         fields,
