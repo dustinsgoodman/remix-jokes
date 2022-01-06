3ff2b4e914bfc70365a40291e4c2d61309e59b64
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-01-05 20:56:56 -0600

    add helper for creating sessino cookie

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import { createCookieSessionStorage, redirect } from "remix";
 import { db } from "./db.server"
 
 type LoginForm = {
@@ -26,4 +27,37 @@ export const login = async ({
   }
 
   return user;
+};
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
 }
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "remix_jokes_session",
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true
+  }
+});
+
+export const createUserSession = async (
+  userId: string,
+  redirectTo: string
+) => {
+  const session = await storage.getSession();
+  session.set('userId', userId);
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  });
+};
